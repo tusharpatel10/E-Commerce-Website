6c7c3a9391334ea0cc5f6577745f1532a851f234
Tushar <tusharpatel1093@gmail.com>
2025-09-02 11:16:35 +0530

    First commit

---
+++ resources/views/layout_user.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Shop Homepage - Start Bootstrap Template</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    @include('layout_header')
+    @yield('content')
+    @include('layout_footer')
+    <!-- Bootstrap core JS-->
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
